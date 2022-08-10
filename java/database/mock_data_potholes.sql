9ee85c7b6f6ec0ebc4a42dc6d9fb18026045994e
paulnguyen <pa.nguyen227@gmail.com>
2022-08-10 08:44:53 -0700

    Adding mock data scripts and E/R diagram.

---
+++ java/database/mock_data_potholes.sql
@@ -0,0 +1,14 @@
+INSERT INTO potholes(pothole_id, lat, long, pothole_name, account_id, image_id, category_id, status_id, active, upload_time)
+VALUES (1, 66.22305259862054, -47.42738921380149, 'hole1', 861, NULL, NULL, NULL, true, '2022-10-20 02:33:21');
+
+INSERT INTO potholes(pothole_id, lat, long, pothole_name, account_id, image_id, category_id, status_id, active, upload_time)
+VALUES (2, 71.44430649416961, -47.619732246950576, 'hole2', 1523, NULL, NULL, NULL, true, '2022-07-11 11:42:11');
+
+INSERT INTO potholes(pothole_id, lat, long, pothole_name, account_id, image_id, category_id, status_id, active, upload_time)
+VALUES (3, -21.092167212293845, -34.22559752463329, 'hole3', 9641, NULL, NULL, NULL, true, '2022-08-08 06:30:18');
+
+INSERT INTO potholes(pothole_id, lat, long, pothole_name, account_id, image_id, category_id, status_id, active, upload_time)
+VALUES (4, 23.35438685747056, 7.940629190860619, 'hole4', 7053, NULL, NULL, NULL, true, '2022-07-29 10:10:10');
+
+INSERT INTO potholes(pothole_id, lat, long, pothole_name, account_id, image_id, category_id, status_id, active, upload_time)
+VALUES (5, 64.76222507757532, -35.96777044179649, 'hole5', 3081, NULL, NULL, NULL, true, '2022-08-04 03:30:33');
---
+++ java/database/mock_data_users.sql
@@ -0,0 +1,29 @@
+INSERT INTO users(user_id, username, password_hash, role)
+VALUES (862, 'graigsatterfield', 'Gs1p9rjVi6', 'USER');
+
+INSERT INTO user_accounts(account_id, user_id, f_name, l_name, email, phone, flagged)
+VALUES (861, 862, 'Graig', 'Satterfield', 'graig.satterfield@gmail.com', 14252437685, false);
+
+INSERT INTO users(user_id, username, password_hash, role)
+VALUES (1524, 'sherlynschimmel', '4Q5swTeEpk', 'USER');
+
+INSERT INTO user_accounts(account_id, user_id, f_name, l_name, email, phone, flagged)
+VALUES (1523, 1524, 'Sherlyn', 'Schimmel', 'sherlyn.schimmel@email.com', 12912797119, false);
+
+INSERT INTO users(user_id, username, password_hash, role)
+VALUES (9642, 'federicoshields', 'vG3r5eLwSC', 'USER');
+
+INSERT INTO user_accounts(account_id, user_id, f_name, l_name, email, phone, flagged)
+VALUES (9641, 9642, 'Federico', 'Shields', 'federico.shields@email.com', 11902491160, false);
+
+INSERT INTO users(user_id, username, password_hash, role)
+VALUES (7054, 'malcolmmann', 'AWXk5KN2wt', 'USER');
+
+INSERT INTO user_accounts(account_id, user_id, f_name, l_name, email, phone, flagged)
+VALUES (7053, 7054, 'Malcolm', 'Mann', 'malcolm.mann@email.com', 19647347305, false);
+
+INSERT INTO users(user_id, username, password_hash, role)
+VALUES (3082, 'dianacrona', 'xWeR8i5b0s', 'USER');
+
+INSERT INTO user_accounts(account_id, user_id, f_name, l_name, email, phone, flagged)
+VALUES (3081, 3082, 'Diana', 'Crona', 'diana.crona@email.com', 19674921935, false);
