d42ae961458e2b7a9d6db18883b20a99daad3b19
Jonathan Ward <jward6t@gmail.com>
2022-08-13 13:54:24 -0500

    Fixing user login and account registration issue

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="register" class="text-center">
-    <form class="form-register" @submit.prevent="register">
+    <form class="form-register" v-if="firstPage">
       <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
@@ -39,16 +39,27 @@
           required
         />
       </div>
-
-
-      <!-- Split below into CreateAccount -->
+      <div>
+        <button class="btn btn-lg btn-primary btn-block" @click.prevent="flipPage">
+          Continue
+        </button>  
+      </div>
+      <div>
+        <router-link :to="{ name: 'login' }">Have an account?</router-link>
+      </div>  
+    </form>
+    <form class="form-register" v-if="!firstPage">
+      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+      <div class="alert alert-danger" role="alert" v-if="accountCreationErrors">
+        {{ accountCreationErrorMsg }}
+      </div>
       <div>
         <label for="first-name" class="sr-only">First Name</label>
         <input
           id="first-name"
           class="form-control"
           placeholder="First Name"
-          v-model="user.firstName"
+          v-model="userAccount.firstName"
           required
         />
       </div>
@@ -58,7 +69,7 @@
           id="last-name"
           class="form-control"
           placeholder="Last Name"
-          v-model="user.lastName"
+          v-model="userAccount.lastName"
           required
         />
       </div>
@@ -69,34 +80,33 @@
           id="email"
           class="form-control"
           placeholder="Email"
-          v-model="user.email"
+          v-model="userAccount.email"
           required
         />
       </div>
       <div>
-        <label for="phoneNumber" class="sr-only">Phone Number</label>
+        <label for="phone" class="sr-only">Phone Number</label>
           <input
-          id="phoneNumber"
+          type="number"
+          id="phone"
           class="form-control"
           placeholder="Phone Number"
-          v-model="user.phoneNumber"
+          v-model="userAccount.phone"
           required
         />
       </div>
       <div>
-        <router-link :to="{ name: 'login' }">Have an account?</router-link>
-      </div>
-      <div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">
-          Create Account
+        <button class="btn btn-lg btn-primary btn-block" @click.prevent="createAccountInfo">
+          Submit
         </button>  
-      </div>      
+      </div>            
     </form>
   </div>
 </template>
 
 <script>
 import authService from '../services/AuthService';
+import accountService from '../services/AccountService';
 
 export default {
   name: 'register',
@@ -107,37 +117,80 @@ export default {
         password: '',
         confirmPassword: '',
         role: 'user',
+      },
+      userAccount: {
+        userId: '',
         firstName: '',
-        lastName: ''
+        lastName: '',
+        email: '',
+        phone: ''
       },
       registrationErrors: false,
       registrationErrorMsg: 'There were problems registering this user.',
+      accountCreationErrors: false,
+      accountCreationErrorMsg: 'There were problems creating an account for this user.',
+      firstPage: true,
+      formComplete: false
     };
   },
   methods: {
-    register() {
-      if (this.user.password !== this.user.confirmPassword) {
-        this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+    checkForm() {
+      if (this.user.username !== '' && this.user.password !== '' && this.user.confirmPassword !== '') {
+        this.formComplete = true;
       } else {
-        authService
-          .register(this.user)
-          .then((response) => {
-            if (response.status === 201) {
-              this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
-              });
-            }
-          })
-          .catch((error) => {
-            const response = error.response;
-            this.registrationErrors = true;
-            if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
-            }
-          });
+        this.formComplete = false;
       }
+    },
+    flipPage() {
+      this.checkForm();
+      if (this.formComplete === false) {
+        this.registrationErrors = true;
+        this.registrationErrorMsg = 'Please fill out all fields.';
+      } else {
+        if (this.user.password !== this.user.confirmPassword) {
+          this.registrationErrors = true;
+          this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        } else {
+          this.register();
+        }
+      }
+    },
+    register() {
+      authService
+        .register(this.user)
+        .then((response) => {
+          if (response.status === 201) {
+            this.userAccount.userId = response.data.id;
+            this.firstPage = false;
+          }
+          })
+        .catch((error) => {
+          const response = error.response;
+          this.registrationErrors = true;
+          if (response.status === 400) {
+            this.registrationErrorMsg = 'Bad Request: Validation Errors';
+          }
+        });
+      return true;
+    },
+    createAccountInfo() {
+      accountService
+        .createAccount(this.userAccount)
+        .then((response) => {
+          if (response.status === 201) {
+            this.$router.push({
+              path: '/login',
+              query: { registration: 'success' },
+            });
+          }
+        })
+        .catch((error) => {
+          const response = error.response;
+          this.accountCreationErrors = true;
+          if (response.status === 400) {
+            this.accountCreationErrorMsg = 'Bad Request: Account Registration Error';
+          }
+        });
     },
     clearErrors() {
       this.registrationErrors = false;
