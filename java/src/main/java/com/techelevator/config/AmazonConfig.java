8e664cb445699dec60369e1e8acc8b055b5a26bc
Brian Steffes <brian.p.steffes@gmail.com>
2022-08-22 11:03:09 -0700

    encrypting amazon credentials

--- java/src/main/java/com/techelevator/config/AmazonConfig.java
+++ java/src/main/java/com/techelevator/config/AmazonConfig.java
@@ -5,20 +5,22 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
+    @Value("{aws.access-key}")
+    private String awsAccessKey;
+    @Value("{aws.secret-key}")
+    private String awsSecretKey;
+
     @Bean
     public AmazonS3 s3() {
 
-        // TODO: Encrypt these credentials
-        AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIAZLFKVTL7MQZX5B6X",
-                "/oDR1B4A7lUvdBjIx7SW7kQsJz6ZO0dO+pcpytUV"
-        );
+        AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
 
         return AmazonS3ClientBuilder
                 .standard()
